e60640ce8fc07e8bfaa231942977757bcd99f77b
MeanBeanie <itsawildveg@gmail.com>
2025-05-17 19:19:24 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# toml-config
+
+header-only library that loads a toml file and turns it into a easy to use struct to store values instead of using `#define` a bunch, plus you wont have to recompile for updates.
+each table is used as a different section of constants, and lets the user use the same name for different values (see `config_section_get(<section>, <key>)`)
+
+designed to reload the config every run, so code would prefferably run:
+```c
+// <includes>
+#define CONFIG_IMPLEMENTATION
+#include <config.h>
+
+int main(void){
+    Config config = config_load("./path/to/config.toml");
+    // rest of code ...
+}
+```
+
+### more info
+
+the library uses a special `Value` type to get data back, which contains a union for the data and an enum for the datatype
+
+there is an efficiency loss as to get anything it uses a linear search which is rather slow, but i will maybe fix that later
+
+### example
+
+```toml
+# config.toml
+[screen]
+width = 400
+height = 400
+title = "Window Title"
+
+[box]
+width = 10
+height = 10
+gravity = 9.8
+name = "Box"
+```
+```c
+// main.c
+#include <stdio.h>
+
+int main(void){
+    Config config = config_load("config.toml");
+
+    printf("Screen Title: %s\n", config_section_get(config, "screen", "title"));
+    printf("Screen Height: %d\n", config_section_get(config, "screen", "height").as.integer);
+    Value value = config_section_get(config, "box", "gravity");
+    if(value.type == VALUE_DOUBLE){
+        printf("Box uses %f for gravity\n", value.as.decimal);
+    }
+    return 0;
+}
+```
---
+++ config.h
@@ -0,0 +1,189 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <stddef.h>
+
+enum ValueType {
+	VALUE_INT,
+	VALUE_DOUBLE,
+	VALUE_STRING,
+};
+
+typedef struct {
+	enum ValueType type;
+	union {
+		int integer;
+		double decimal;
+		char* string;
+	} as;
+	char name[64];
+	char section[64];
+} Value;
+
+
+typedef struct {
+	Value* values;
+	size_t size;
+	size_t capacity;
+} Config;
+
+static Config config_load(const char* path);
+static Value config_section_get(Config config, const char* section, const char* name);
+
+#endif // CONFIG_H
+
+#ifdef CONFIG_IMPLEMENTATION
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+static Config config_load(const char* path){
+	FILE* file = fopen(path, "r");
+	if(file == NULL){
+		perror("CONFIG ERROR: config_load: failed to open file");
+		return (Config){.values = NULL, .size = 0, .capacity = 0};
+	}
+
+	fseek(file, 0, SEEK_END);
+	size_t size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	char buffer[size];
+	fread(buffer, sizeof(char), size, file);
+	fclose(file);
+
+	Config res = {
+		.size = 0,
+		.capacity = 8,
+		.values = malloc(8*sizeof(Value))
+	};
+
+	bool inWord = false;
+	int wordSize = 0;
+	char currentSection[64] = {0};
+	char currentName[64] = {0};
+	int stage = 0;
+	for(size_t i = 0; i < size; i++){
+		char c = buffer[i];
+		if(inWord){
+			if(c == ']'){
+				memset(currentSection, 0, 64);
+				strncpy(currentSection, buffer+i-wordSize, wordSize);
+				inWord = false;
+				continue;
+			}
+			else if(stage == 0 && c == ' '){
+				memset(currentName, 0, 64);
+				strncpy(currentName, buffer+i-wordSize-1, wordSize+1);
+				inWord = false;
+				stage++;
+				continue;
+			}
+			else if(c == '\n'){
+				Value value = {0};
+				strncpy(value.name, currentName, 64);
+				strncpy(value.section, currentSection, 64);
+				
+				char starting = buffer[i-wordSize+1];
+				if(starting == '"'){
+					value.type = VALUE_STRING;
+					value.as.string = malloc(sizeof(char)*(wordSize+2));
+					strncpy(value.as.string, buffer+i-wordSize+2, wordSize-3);
+					value.as.string[wordSize+2] = '\0';
+				}
+				else if(starting >= '0' && starting <= '9'){
+					value.type = VALUE_INT;
+					for(int j = 0; j < wordSize+1; j++){
+						if(buffer[i-wordSize-1+j] == '.'){
+							value.type = VALUE_DOUBLE;
+						}
+					}
+
+					if(value.type == VALUE_INT){
+						int integer = 0;
+						for(int j = 0; j < wordSize-1; j++){
+							int p10 = wordSize-j-2;
+							int shift = 1;
+							for(int k = 0; k < p10; k++){
+								shift *= 10;
+							}
+							integer += ((buffer[i-wordSize+1+j] - '0') * shift);
+						}
+						value.as.integer = integer;
+					}
+					else{
+						int lhs = 0;
+						int rhs = 0;
+						int index = 0;
+						for(int j = 0; j < wordSize-1; j++){
+							if(buffer[i-wordSize+1+j] == '.'){
+								index = j;
+							}
+						}
+						int integer = 0;
+						for(int j = 0; j < index; j++){
+							int p10 = index-j-1;
+							int shift = 1;
+							for(int k = 0; k < p10; k++){
+								shift *= 10;
+							}
+							integer += ((buffer[i-wordSize+1+j] - '0') * shift);
+						}
+						int decimal = 0;
+						for(int j = index+1; j < wordSize-1; j++){
+							int p10 = wordSize-j-2;
+							int shift = 1;
+							for(int k = 0; k < p10; k++){
+								shift *= 10;
+							}
+							decimal += ((buffer[i-wordSize+1+j] - '0') * shift);
+						}
+						int shift = 1;
+						for(int j = 0; j < (wordSize-2-index); j++){
+							shift *= 10;
+						}
+						value.as.decimal = integer + (double)decimal/shift;
+					}
+				}
+
+				if(res.size >= res.capacity){
+					res.capacity *= 2;
+					res.values = realloc(res.values, res.capacity*sizeof(Value));
+					if(res.values == NULL){
+						fprintf(stderr, "Ran out of memory for the config values, there are too many\n");
+						return (Config){.values = NULL, .size = 0, .capacity = 0};
+					}
+				}
+				res.values[res.size] = value;
+				res.size++;
+
+				inWord = false;
+				stage = 0;
+				continue;
+			}
+			wordSize++;
+			continue;
+		}
+		if(c >= 0x21 && c <= 0x7e){
+			wordSize = 0;
+			inWord = true;
+		}
+	}
+
+	return res;
+}
+
+static Value config_section_get(Config config, const char* section, const char* name){
+	Value res = {0};
+
+	for(int i = 0; i < config.size; i++){
+		if(strncmp(config.values[i].section, section, strlen(section)) == 0
+		&& strncmp(config.values[i].name, name, strlen(name)) == 0){
+			res = config.values[i];
+		}
+	}
+
+	return res;
+}
+
+#endif // CONFIG_IMPLEMENTATION
